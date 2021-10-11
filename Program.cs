898789df503fa08dfbf8c37e92791cf4e78aa616
Ronny Soltveit <ronny@soltveit.org>
2021-10-12 01:53:01 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;    // Used for streamreader
+using System.Net;
+using Renci.SshNet;
+
+namespace IpUploader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("IpUploader");
+            string publicIP = MyIP();
+            string outputPath = "public-ip.txt";
+            Console.WriteLine("Public IP: " + publicIP);
+
+            string strToWrite = "Time: " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "\n" + "IPv4: " + publicIP + "\n";
+            WriteToFile(outputPath, strToWrite);
+            Console.WriteLine("Uploading IP to server...");
+            SendFileToServer.Send(outputPath);
+        }
+
+        public static string MyIP()
+        {
+            string externalip = new WebClient().DownloadString("https://soltveit.org/pages/myip/");
+
+            return externalip;
+        }
+
+        public static void WriteToFile(string path, string toWrite)
+        {
+            Console.WriteLine("Writing ip to file");
+            File.WriteAllText(path, toWrite);
+        }
+
+        public static string[] GetConfig(string path)
+        {
+            string[] config = File.ReadAllLines(path);  // Check README for more info
+
+            return config;
+        }
+
+        public static class SendFileToServer
+        {
+            public static int Send(string fileName)
+            {
+                string[] config = GetConfig("config.txt");
+                var connectionInfo = new ConnectionInfo(config[0], config[1], new PasswordAuthenticationMethod(config[1], config[2]));
+                using (var sftp = new SftpClient(connectionInfo))
+                {
+                    try
+                    {
+                        sftp.Connect();
+                        sftp.ChangeDirectory(config[3]);
+                        using (var uplfileStream = File.OpenRead(fileName))
+                        {
+                            sftp.UploadFile(uplfileStream, fileName, true);
+                        }
+                        sftp.Disconnect();
+                        Console.WriteLine("Done....\nDisconnected...\n");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                return 0;
+            }
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,75 @@
+# IP Uploader
+
+IP Loader saves the public ip of the machine it is run from, and uploads it to a ssh host.<br>
+Where only me, Google and NSA has access to it.<br>
+
+It is basicly the exact same tool as Public IP Tool, but this is for .net core 3 instead of .net framwork.<br>
+
+It is sending a webrequest to https://soltveit.org/pages/myip/ and take the ip from<br>
+the response.<br>
+
+This is how the response looks like:<br>
+```
+92.250.85.90
+```
+After the program got the ip address it saves it as a text file (public-ip.txt), and upload it to the
+server and path specified in the config file.<br>
+
+How the output file looks like:
+```
+Time: 2019-10-04 21:46:32
+IPv4: 92.221.70.95
+```
+
+## How to use
+
+To run this every hour, put this in your crontab:<br>
+```
+0 */1 * * * ( cd /home/user/folder && ./ip-tool.sh )
+```
+
+Example of ip-tool.sh<br>
+```
+#!/bin/sh
+dotnet IpUploader.dll
+```
+Remember to make the scipt executable.<br>
+```
+chmod +x ip-tool.sh
+```
+## The code
+There is now a simple config instead of hard coded connection info. It must be in the same folder as the ip uploader
+is run from. You can use hostname or ip address. It both works.<br>
+
+Example of the config.txt. There is also a copy in the project root folder.<br>
+```
+my.server.com
+ronny
+Password
+/home/ronny/public-ip
+```
+
+The public ip is optained by sending a web request to https://soltveit.org/pages/myip/
+```
+public static string MyIP()
+{
+    string externalip = new WebClient().DownloadString("https://soltveit.org/pages/myip/");
+
+    return externalip;
+}
+```
+
+Added this php script to my web server:<br>
+
+<strong>ip.php</strong><br>
+```
+<?php
+echo $_SERVER['REMOTE_ADDR'];
+?>
+```
+
+A successful run:<br>
+![alt public ip screenshot](http://web.home/pics/gitlab/iptool.jpg)
+
+## TODO
+- Encrypt password in config.txt<br>
